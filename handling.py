76635874f883226f76bf0a412b65bb0db956515e
sadana.vaibhav@gmail.com <vaibhav9619>
2018-07-04 10:50:36 +0530

    Error handling code

---
+++ handling.py
@@ -0,0 +1,20 @@
+
+try:
+    i=0/0
+    f=open("abc.txt")
+    for line in f:
+        print(line)
+
+# except:
+#     print("why with me")
+except Exception as e:
+    print(e)
+
+# except FileNotFoundError:
+#     print("file not found")
+# except ZeroDivisionError:
+#     print("divided by 0 bro")
+# except (FileNotFoundError,ZeroDivisionError):
+#     print("File not Found adsdsxdas")
+
+
